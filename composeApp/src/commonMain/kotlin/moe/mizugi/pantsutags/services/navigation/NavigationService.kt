251d1f3eca2fb9615ae0dd3d3042833c0df974b3
Joris Diem <j.diem@me.com>
2025-10-13 23:00:10 +0200

    add logger framework and example logging in NavigationService

--- composeApp/src/commonMain/kotlin/moe/mizugi/pantsutags/services/navigation/NavigationService.kt
+++ composeApp/src/commonMain/kotlin/moe/mizugi/pantsutags/services/navigation/NavigationService.kt
@@ -1,13 +1,16 @@
 package moe.mizugi.pantsutags.services.navigation
 
 import androidx.navigation.NavController
+import io.github.oshai.kotlinlogging.KotlinLogging
 import moe.mizugi.pantsutags.AppRoute
+
+private val logger = KotlinLogging.logger {}
 
 class NavigationService {
     private var navController: NavController? = null
 
     fun navigateTo(route: AppRoute) {
-        this.navController?.navigate(route)
+        this.navController?.navigate(route) ?: logger.error { "NavController is not set" }
     }
 
     fun setNavController(navController: NavController) {
