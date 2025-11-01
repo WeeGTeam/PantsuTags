09bfe04d8c546957c4fef8d43eb0832fb2144730
Joris Diem <j.diem@me.com>
2025-11-01 18:10:02 +0100

    add padding around AppScreen content

--- composeApp/src/jvmMain/kotlin/moe/mizugi/pantsutags/AppScreen.jvm.kt
+++ composeApp/src/jvmMain/kotlin/moe/mizugi/pantsutags/AppScreen.jvm.kt
@@ -1,15 +1,20 @@
 package moe.mizugi.pantsutags
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import moe.mizugi.pantsutags.presentation.navigation.SideNavigation
 
 @Composable
 actual fun AppScreen(content: @Composable (() -> Unit)) {
     Row(modifier = Modifier.safeContentPadding()) {
         SideNavigation()
-        content()
+        Box(modifier = Modifier.padding(5.dp)) {
+            content()
+        }
     }
 }
