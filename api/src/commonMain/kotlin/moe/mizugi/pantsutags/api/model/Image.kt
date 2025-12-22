c539faa090128573d7bbe4b5c1f768d706b4e80c
Joris Diem <j.diem@me.com>
2025-12-22 23:38:54 +0100

    add openapi tools generator setup
    Convert image (octet stream) to bytearray.
    Otherwise, the code generator tries
    and fails to interpret the image as string.

---
+++ api/src/commonMain/kotlin/moe/mizugi/pantsutags/api/model/Image.kt
@@ -0,0 +1,24 @@
+package moe.mizugi.pantsutags.api.model
+
+data class Image(
+    val id: String,
+    val data: ByteArray
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as Image
+
+        if (id != other.id) return false
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + data.contentHashCode()
+        return result
+    }
+}
