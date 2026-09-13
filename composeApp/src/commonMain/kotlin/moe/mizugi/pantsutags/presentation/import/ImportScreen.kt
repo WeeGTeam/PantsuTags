110be2b0da054cbc7d479dfdaf9fc5d86aabf2fd
Joris Diem <j.diem@me.com>
2026-09-13 18:56:07 +0200

    fix renamed image repository
    Signed-off-by: Joris Diem <j.diem@me.com>

--- composeApp/src/commonMain/kotlin/moe/mizugi/pantsutags/presentation/import/ImportScreen.kt
+++ composeApp/src/commonMain/kotlin/moe/mizugi/pantsutags/presentation/import/ImportScreen.kt
@@ -3,16 +3,16 @@ package moe.mizugi.pantsutags.presentation.import
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.*
 import com.composeunstyled.Text
-import moe.mizugi.pantsutags.api.repository.ImageListRepository
+import moe.mizugi.pantsutags.api.repository.ImageRepository
 import org.koin.compose.koinInject
 
 @Composable
 fun ImportScreen(
-    imageListRepository: ImageListRepository = koinInject(),
+    imageRepository: ImageRepository = koinInject(),
 ) {
     var imageIds by remember { mutableStateOf<List<String>>(listOf()) }
     LaunchedEffect(Unit) {
-        val result = imageListRepository.getImages();
+        val result = imageRepository.getImages();
         imageIds = result.getOrElse { listOf() }
     }
 
